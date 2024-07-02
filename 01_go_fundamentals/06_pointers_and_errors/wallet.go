b0716e18d9e778818f9a9d937f4a7849a29b8440
xanish <danishali7866@gmail.com>
2024-07-02 23:55:45 +0530

    06-v2: wallet with bitcoin type and implement Stringer on bitcoin

--- 01_go_fundamentals/06_pointers_and_errors/wallet.go
+++ 01_go_fundamentals/06_pointers_and_errors/wallet.go
@@ -1,15 +1,23 @@
 package _6_pointers_and_errors
 
+import "fmt"
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
 	// In Go if a symbol (variables, types, functions et al) starts with a lowercase symbol then it is private
 	// outside the package it's defined in.
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Balance() int {
-	return w.balance
-}
-
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
 }
--- 01_go_fundamentals/06_pointers_and_errors/wallet_test.go
+++ 01_go_fundamentals/06_pointers_and_errors/wallet_test.go
@@ -5,12 +5,12 @@ import "testing"
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
