046b4a5a385fa00ba4fb2b02068f641fa02d674b
xanish <danishali7866@gmail.com>
2024-07-02 23:11:12 +0530

    05-v3: support area and permiter of circle

--- 01_go_fundamentals/05_structs_methods_and_interfaces/shapes.go
+++ 01_go_fundamentals/05_structs_methods_and_interfaces/shapes.go
@@ -1,13 +1,27 @@
 package _5_structs_methods_and_interfaces
 
+import "math"
+
 type Rectangle struct {
 	Width, Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2*rectangle.Width + 2*rectangle.Height
+func (r Rectangle) Perimeter() float64 {
+	return 2*r.Width + 2*r.Height
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- 01_go_fundamentals/05_structs_methods_and_interfaces/shapes_test.go
+++ 01_go_fundamentals/05_structs_methods_and_interfaces/shapes_test.go
@@ -3,21 +3,45 @@ package _5_structs_methods_and_interfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Perimeter()
+		want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Perimeter()
+		want := 62.83185307179586
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
