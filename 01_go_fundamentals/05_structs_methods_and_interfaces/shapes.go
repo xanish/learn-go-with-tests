a2da6023aeb99b51880e35f99f6d84c2d67113c3
xanish <danishali7866@gmail.com>
2024-07-02 23:00:07 +0530

    05-v1: area and perimeter of rectangle

---
+++ 01_go_fundamentals/05_structs_methods_and_interfaces/shapes.go
@@ -0,0 +1,9 @@
+package _5_structs_methods_and_interfaces
+
+func Perimeter(width float64, height float64) float64 {
+	return 2*width + 2*height
+}
+
+func Area(width float64, height float64) float64 {
+	return width * height
+}
