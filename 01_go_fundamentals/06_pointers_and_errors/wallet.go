04290772bd9cf4ac478f0646825df7194ffa65da
xanish <danishali7866@gmail.com>
2024-07-03 00:01:16 +0530

    06-v4: handle edge case withrdraw function

--- 01_go_fundamentals/06_pointers_and_errors/wallet.go
+++ 01_go_fundamentals/06_pointers_and_errors/wallet.go
@@ -1,6 +1,9 @@
 package _6_pointers_and_errors
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -22,6 +25,11 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+
 	w.balance -= amount
+	return nil
 }
