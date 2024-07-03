9a3907ac46b95066b98a9bc70edc106da3ff9ed1
xanish <danishali7866@gmail.com>
2024-07-03 23:32:49 +0530

    09-v2: mock sleep() in countdown for faster testing

--- 01_go_fundamentals/09_mocking/countdown.go
+++ 01_go_fundamentals/09_mocking/countdown.go
@@ -3,17 +3,33 @@ package _9_mocking
 import (
 	"fmt"
 	"io"
+	"os"
 	"time"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		_, _ = fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 
 	_, _ = fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
