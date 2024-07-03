cf7f59126e1aa741dd9b3fadb98cc18900365107
xanish <danishali7866@gmail.com>
2024-07-03 22:25:20 +0530

    07-v3: handle non-existent word

--- 01_go_fundamentals/07_maps/dictionary_test.go
+++ 01_go_fundamentals/07_maps/dictionary_test.go
@@ -1,14 +1,29 @@
 package _7_maps
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+		if got == nil {
+			t.Fatal("expected to get an error.")
+		}
+		if !errors.Is(got, ErrNotFound) {
+			t.Errorf("got error %q want %q", got, ErrNotFound)
+		}
+	})
 }
