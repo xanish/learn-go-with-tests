71ae17dc44775d6ca48b8432ae2d8ed8297e225a
xanish <danishali7866@gmail.com>
2024-07-03 22:40:12 +0530

    07-v5: clean up errors

--- 01_go_fundamentals/07_maps/dictionary.go
+++ 01_go_fundamentals/07_maps/dictionary.go
@@ -4,10 +4,16 @@ import (
 	"errors"
 )
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 type Dictionary map[string]string
 
