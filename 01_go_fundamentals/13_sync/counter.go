f69fc02b94f3b49de485cf3545bb4867b4e3ada5
xanish <danishali7866@gmail.com>
2024-07-04 22:04:24 +0530

    13-v2: counter with mutex and concurrent updates

--- 01_go_fundamentals/13_sync/counter.go
+++ 01_go_fundamentals/13_sync/counter.go
@@ -1,13 +1,26 @@
 package _3_sync
 
+import "sync"
+
+// Counter will increment a number.
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
+// NewCounter returns a new Counter.
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+// Inc will increment the count.
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
+// Value returns the current count.
 func (c *Counter) Value() int {
 	return c.value
 }
