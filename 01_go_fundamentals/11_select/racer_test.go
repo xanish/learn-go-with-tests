9ea3a06ccd70d88db11c8b5ee7944dedf5cc8ba1
xanish <danishali7866@gmail.com>
2024-07-04 21:23:44 +0530

    11-v2: mocking http server

--- 01_go_fundamentals/11_select/racer_test.go
+++ 01_go_fundamentals/11_select/racer_test.go
@@ -1,10 +1,24 @@
 package _1_select
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got, err := Racer(slowURL, fastURL)
@@ -15,4 +29,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
