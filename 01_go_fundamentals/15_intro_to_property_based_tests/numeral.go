a58c8b67e07c1015f3880efa017a315cbdb1d98e
xanish <danishali7866@gmail.com>
2024-07-05 22:23:00 +0530

    15-v1: int to roman

---
+++ 01_go_fundamentals/15_intro_to_property_based_tests/numeral.go
@@ -0,0 +1,43 @@
+package _5_intro_to_property_based_tests
+
+import "strings"
+
+// RomanNumeral represents a mapping between an integer value and its Roman numeral symbol.
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+// RomanNumerals is a predefined slice of RomanNumeral structs that maps integers to Roman numeral symbols.
+var RomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+// ConvertToRoman converts an integer (arabic) into its Roman numeral representation.
+func ConvertToRoman(arabic int) string {
+	// Initialize a strings.Builder to efficiently construct the result.
+	var result strings.Builder
+
+	for _, numeral := range RomanNumerals {
+		// Append the Roman numeral symbol to the result repeatedly till the arabic number is
+		// greater than or equal to the current numeral's value.
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
