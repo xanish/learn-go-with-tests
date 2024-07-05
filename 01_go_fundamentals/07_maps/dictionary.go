51b96c2bf94256b44017745d032746a87ecc9be0
xanish <danishali7866@gmail.com>
2024-07-05 21:45:51 +0530

    07-v8: add comments

--- 01_go_fundamentals/07_maps/dictionary.go
+++ 01_go_fundamentals/07_maps/dictionary.go
@@ -4,20 +4,25 @@ import (
 	"errors"
 )
 
-const (
-	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
-	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
-)
-
+// DictionaryErr represents custom error messages related to dictionary operations.
 type DictionaryErr string
 
 func (e DictionaryErr) Error() string {
 	return string(e)
 }
 
+// Predefined error constants for dictionary operations.
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+// Dictionary is a map that holds word definitions.
 type Dictionary map[string]string
 
+// Search looks up a word in the dictionary and returns its definition.
+// Returns ErrNotFound if the word is not found.
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
@@ -27,6 +32,8 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
+// Add inserts a new word and its definition into the dictionary.
+// Returns ErrWordExists if the word already exists.
 func (d Dictionary) Add(word, definition string) error {
 	_, err := d.Search(word)
 
@@ -42,6 +49,8 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
+// Update modifies the definition of an existing word in the dictionary.
+// Returns ErrWordDoesNotExist if the word does not exist.
 func (d Dictionary) Update(word, definition string) error {
 	_, err := d.Search(word)
 
@@ -57,6 +66,7 @@ func (d Dictionary) Update(word, definition string) error {
 	return nil
 }
 
+// Delete removes a word and its definition from the dictionary.
 func (d Dictionary) Delete(word string) {
 	delete(d, word)
 }
