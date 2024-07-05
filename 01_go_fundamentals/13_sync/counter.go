a6cc6447663728294802a4d50864838080ad72fc
xanish <danishali7866@gmail.com>
2024-07-05 22:01:40 +0530

    13-v3: add comments

--- 01_go_fundamentals/13_sync/counter.go
+++ 01_go_fundamentals/13_sync/counter.go
@@ -2,8 +2,11 @@ package _3_sync
 
 import "sync"
 
-// Counter will increment a number.
+// Counter will increment a number. This problem could've easily been solved
+// using channels as well but mutexes just make handling this case much cleaner
+// compared to waiting on a channel.
 type Counter struct {
+	// Mutex ensures only one goroutine can increment the counter at a time.
 	mu    sync.Mutex
 	value int
 }
