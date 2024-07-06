1b1f29de46245929b34c23a0ae7f7563672cc178
xanish <danishali7866@gmail.com>
2024-07-06 12:25:25 +0530

    20-v3: generic find implementation

--- 01_go_fundamentals/20_revisiting_arrays_and_slices_with_generics/collections.go
+++ 01_go_fundamentals/20_revisiting_arrays_and_slices_with_generics/collections.go
@@ -1,5 +1,14 @@
 package _0_revisiting_arrays_and_slices_with_generics
 
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, v := range items {
+		if predicate(v) {
+			return v, true
+		}
+	}
+	return
+}
+
 func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
 	var result = initialValue
 	for _, x := range collection {
--- 01_go_fundamentals/20_revisiting_arrays_and_slices_with_generics/collections_test.go
+++ 01_go_fundamentals/20_revisiting_arrays_and_slices_with_generics/collections_test.go
@@ -1,6 +1,40 @@
 package _0_revisiting_arrays_and_slices_with_generics
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(x int) bool {
+			return x%2 == 0
+		})
+		AssertTrue(t, found)
+		AssertEqual(t, firstEvenNumber, 2)
+	})
+
+	type Person struct {
+		Name string
+	}
+
+	t.Run("Find the best programmer", func(t *testing.T) {
+		people := []Person{
+			Person{Name: "Kent Beck"},
+			Person{Name: "Martin Fowler"},
+			Person{Name: "Chris James"},
+		}
+
+		king, found := Find(people, func(p Person) bool {
+			return strings.Contains(p.Name, "Chris")
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, king, Person{Name: "Chris James"})
+	})
+}
 
 func TestReduce(t *testing.T) {
 	t.Run("multiplication of all elements", func(t *testing.T) {
