7b8c93215299f2be3c4680d2edd822a7f673c80f
xanish <danishali7866@gmail.com>
2024-07-06 22:07:27 +0530

    12-v2: reflection walk handle all fields

--- 01_go_fundamentals/12_reflection/reflection.go
+++ 01_go_fundamentals/12_reflection/reflection.go
@@ -6,12 +6,13 @@ func walk(x interface{}, fn func(input string)) {
 	// The "reflect" package has a function ValueOf which returns us a Value of a given variable.
 	val := reflect.ValueOf(x)
 
-	// We then make some very optimistic assumptions about the value passed in:
-	// We look at the first and only field. However, there may be no fields at all,
-	// which would cause a panic.
-	field := val.Field(0)
+	for i := 0; i < val.NumField(); i++ {
+		// We then make some very optimistic assumptions about the value passed in:
+		// We check all values one by one. However, there may be no fields at all, which would cause a panic.
+		field := val.Field(i)
 
-	// We then call String(), which returns the underlying value as a string. However, this
-	// would be wrong if the field was something other than a string.
-	fn(field.String())
+		// We then call String(), which returns the underlying value as a string. However, this
+		// would be wrong if the field was something other than a string.
+		fn(field.String())
+	}
 }
