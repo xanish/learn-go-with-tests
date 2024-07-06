5b8c1faeac6fd544224ba194fb205f6d3cc58fdf
xanish <danishali7866@gmail.com>
2024-07-06 12:23:17 +0530

    20-v2: allow reduce to work on 2 different types

--- 01_go_fundamentals/20_revisiting_arrays_and_slices_with_generics/collections.go
+++ 01_go_fundamentals/20_revisiting_arrays_and_slices_with_generics/collections.go
@@ -1,6 +1,6 @@
 package _0_revisiting_arrays_and_slices_with_generics
 
-func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
 	var result = initialValue
 	for _, x := range collection {
 		result = f(result, x)
